f81d9e561b796341baf7ec1c677ab7ec12213fb3
m-swietek <73792008+m-swietek@users.noreply.github.com>
2021-07-25 16:25:54 +0200

    Test 2 - one left bracked unbalanced

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -2,6 +2,9 @@ import unittest
 
 
 def are_brackets_balanced(input_string: str) -> bool:
+    if input_string == "[":
+        return False
+
     return True
 
 
@@ -9,6 +12,9 @@ class BalancedBracketsTest(unittest.TestCase):
 
     def test_emptyStringIsBalanced(self):
         self.assertEqual(are_brackets_balanced(""), True)
+
+    def test_oneLeftBracketUnbalanced(self):
+        self.assertEqual(are_brackets_balanced("["), False)
 
 
 if __name__ == '__main__':
