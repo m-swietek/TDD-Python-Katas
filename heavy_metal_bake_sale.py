c0c4f8fed825dd99471883717a5401beed8d80d8
m-swietek <73792008+m-swietek@users.noreply.github.com>
2021-07-29 13:31:15 +0200

    Full sell scenarios tests

--- heavy_metal_bake_sale.py
+++ heavy_metal_bake_sale.py
@@ -13,20 +13,25 @@ class BakeSale:
         self.cakePopQuantity = cake_pops
         self.waterQuantity = waters
         self.totalToPay = 0.0
+        self.amountPaid = 0.0
+        self.browniesToSell = 0
+        self.muffinsToSell = 0
+        self.cakePopsToSell = 0
+        self.watersToSell = 0
 
     def get_brownie_quantity(self) -> int:
         return self.brownieQuantity
 
-    def get_water_quantity(self):
+    def get_water_quantity(self) -> int:
         return self.waterQuantity
 
-    def get_cake_pop_quantity(self):
+    def get_cake_pop_quantity(self) -> int:
         return self.cakePopQuantity
 
-    def get_muffin_quantity(self):
+    def get_muffin_quantity(self) -> int:
         return self.muffinQuantity
 
-    def get_total_to_pay(self):
+    def get_total_to_pay(self) -> float:
         return self.totalToPay
 
     def sell_brownie(self, quantity: int):
@@ -34,44 +39,70 @@ class BakeSale:
             self.not_enough_items_warning()
         else:
             self.brownieQuantity -= 1
-            self.totalToPay += self.browniePrice
 
     def sell_muffin(self, quantity: int):
         if quantity > self.muffinQuantity:
             self.not_enough_items_warning()
         else:
             self.muffinQuantity -= 1
-            self.totalToPay += self.muffinPrice
 
     def sell_cake_pop(self, quantity: int):
         if quantity > self.cakePopQuantity:
             self.not_enough_items_warning()
         else:
             self.cakePopQuantity -= 1
-            self.totalToPay += self.cakePopPrice
 
     def sell_water(self, quantity: int):
         if quantity > self.waterQuantity:
             self.not_enough_items_warning()
         else:
             self.waterQuantity -= 1
-            self.totalToPay += self.waterPrice
 
     def not_enough_items_warning(self):
         print("Not enough stock")
+
+    def not_enough_money_warning(self):
+        print("Not enough money")
 
     def parse_input(self, input_string: str):
         items_list = input_string.split(',')
 
         for item in items_list:
             if item == "B":
-                self.sell_brownie(1)
+                self.browniesToSell += 1
+                self.totalToPay += self.browniePrice
             elif item == "M":
-                self.sell_muffin(1)
+                self.muffinsToSell += 1
+                self.totalToPay += self.muffinPrice
             elif item == "C":
-                self.sell_cake_pop(1)
+                self.cakePopsToSell += 1
+                self.totalToPay += self.cakePopPrice
             elif item == "W":
-                self.sell_water(1)
+                self.watersToSell += 1
+                self.totalToPay += self.waterPrice
+
+    def set_amount_paid(self, amount: float):
+        self.amountPaid = amount
+
+    def sell_items(self):
+        if self.amountPaid >= self.totalToPay:
+            if self.browniesToSell > 0:
+                self.sell_brownie(self.browniesToSell)
+                self.browniesToSell = 0
+
+            if self.muffinsToSell > 0:
+                self.sell_muffin(self.muffinsToSell)
+                self.muffinsToSell = 0
+
+            if self.cakePopsToSell > 0:
+                self.sell_cake_pop(self.cakePopsToSell)
+                self.cakePopsToSell = 0
+
+            if self.watersToSell > 0:
+                self.sell_water(self.watersToSell)
+                self.watersToSell = 0
+        else:
+            self.not_enough_money_warning()
 
 
 class MyTestCase(unittest.TestCase):
@@ -106,53 +137,47 @@ class MyTestCase(unittest.TestCase):
         self.sale.sell_brownie(1)
         self.sale.not_enough_items_warning.assert_not_called()
 
-    def test_userInputCausesSellMethodCalls(self):
-        self.sale = BakeSale(0, 0, 0, 0)
-        self.sale.sell_brownie = MagicMock()
-        self.sale.sell_muffin = MagicMock()
-        self.sale.sell_cake_pop = MagicMock()
-        self.sale.sell_water = MagicMock()
-
-        self.sale.parse_input('B')
-        self.sale.sell_brownie.assert_called()
-        self.sale.parse_input('M')
-        self.sale.sell_muffin.assert_called()
-        self.sale.parse_input('C')
-        self.sale.sell_cake_pop.assert_called()
-        self.sale.parse_input('W')
-        self.sale.sell_water.assert_called()
-
-    def test_allItemsInInput(self):
-        self.sale = BakeSale(0, 0, 0, 0)
-        self.sale.sell_brownie = MagicMock()
-        self.sale.sell_muffin = MagicMock()
-        self.sale.sell_cake_pop = MagicMock()
-        self.sale.sell_water = MagicMock()
-
-        self.sale.parse_input('B,M,C,W')
-        self.sale.sell_brownie.assert_called()
-        self.sale.sell_muffin.assert_called()
-        self.sale.sell_cake_pop.assert_called()
-        self.sale.sell_water.assert_called()
-
-    def test_mixedItemsInInput(self):
-        self.sale = BakeSale(0, 0, 0, 0)
-        self.sale.sell_brownie = MagicMock()
-        self.sale.sell_muffin = MagicMock()
-        self.sale.sell_cake_pop = MagicMock()
-        self.sale.sell_water = MagicMock()
-
-        self.sale.parse_input('B,C')
-        self.sale.sell_brownie.assert_called()
-        self.sale.sell_muffin.assert_not_called()
-        self.sale.sell_cake_pop.assert_called()
-        self.sale.sell_water.assert_not_called()
-
     def test_totalGetsCalculatedCorrectly(self):
         self.sale = BakeSale(1, 0, 1, 0)
 
         self.sale.parse_input('B,C')
         self.assertEqual(self.sale.get_total_to_pay(), 2.0)
+
+    def test_mixedItemsRightAmountPaid(self):
+        self.sale = BakeSale(1, 1, 1, 1)
+        self.sale.sell_brownie = MagicMock()
+        self.sale.sell_muffin = MagicMock()
+        self.sale.sell_cake_pop = MagicMock()
+        self.sale.sell_water = MagicMock()
+        self.sale.not_enough_items_warning = MagicMock()
+
+        self.sale.parse_input('B,C')
+        self.sale.set_amount_paid(2.0)
+        self.sale.sell_items()
+
+        self.sale.sell_brownie.assert_called()
+        self.sale.sell_muffin.assert_not_called()
+        self.sale.sell_cake_pop.assert_called()
+        self.sale.sell_water.assert_not_called()
+        self.sale.not_enough_items_warning.assert_not_called()
+
+    def test_mixedItemsNotEnoughMoneyPaid(self):
+        self.sale = BakeSale(1, 1, 1, 1)
+        self.sale.sell_brownie = MagicMock()
+        self.sale.sell_muffin = MagicMock()
+        self.sale.sell_cake_pop = MagicMock()
+        self.sale.sell_water = MagicMock()
+        self.sale.not_enough_money_warning = MagicMock()
+
+        self.sale.parse_input('B,C')
+        self.sale.set_amount_paid(1.5)
+        self.sale.sell_items()
+
+        self.sale.sell_brownie.assert_not_called()
+        self.sale.sell_muffin.assert_not_called()
+        self.sale.sell_cake_pop.assert_not_called()
+        self.sale.sell_water.assert_not_called()
+        self.sale.not_enough_money_warning.assert_called_once()
 
 
 if __name__ == '__main__':
