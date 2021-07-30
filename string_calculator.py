fa57aa5a8059a37a8ffaace8cc1a74f8f5d68b67
m-swietek <73792008+m-swietek@users.noreply.github.com>
2021-07-30 11:02:50 +0200

    String calculator kata - multiple numbers separated by comma get added

--- string_calculator.py
+++ string_calculator.py
@@ -5,7 +5,20 @@ def add(number: str) -> str:
     if number == "":
         return "0"
     else:
-        return number
+        numbers_split = number.split(',')
+
+        if len(numbers_split) == 1:
+            return number
+        else:
+            numbers_sum = 0
+
+            for x in numbers_split:
+                numbers_sum += float(x)
+
+            if numbers_sum.is_integer():
+                return str(int(numbers_sum))
+            else:
+                return str(numbers_sum)
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +28,11 @@ class MyTestCase(unittest.TestCase):
     def test_oneNumberGetsReturned(self):
         self.assertEqual(add("1"), "1")
         self.assertEqual(add("1.1"), "1.1")
+
+    def test_numbersSeparatedByCommaGetAdded(self):
+        self.assertEqual(add("1,2"), "3")
+        self.assertEqual(add("1,2,3"), "6")
+        self.assertEqual(add("1.1,2.3,3.5"), "6.9")
 
 
 if __name__ == '__main__':
