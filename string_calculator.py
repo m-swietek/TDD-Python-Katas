435f12689a63fff4625bece28fcf1792f8ce2fa6
m-swietek <73792008+m-swietek@users.noreply.github.com>
2021-07-30 10:49:01 +0200

    String calculator kata - one number entered gets returned

--- string_calculator.py
+++ string_calculator.py
@@ -2,12 +2,19 @@ import unittest
 
 
 def add(number: str) -> str:
-    return "0"
+    if number == "":
+        return "0"
+    else:
+        return number
 
 
 class MyTestCase(unittest.TestCase):
     def test_emptyStringReturns0(self):
         self.assertEqual(add(""), "0")
+
+    def test_oneNumberGetsReturned(self):
+        self.assertEqual(add("1"), "1")
+        self.assertEqual(add("1.1"), "1.1")
 
 
 if __name__ == '__main__':
